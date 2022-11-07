9bab0db3e60724812ad9dddb8faa8e6d8d66c685
Ovie Okpaka <ovieokpaka@gmail.com>
2022-11-07 00:09:16 -1200

    My first code from my new windows bootcamp system

---
+++ js/dom.js
@@ -0,0 +1,13 @@
+function green(){
+ document.getElementsByClassName('green')[0].style.backgroundcolor = 'green';
+  
+}
+function blue(){
+    document.getElementsByClassName('blue')[0].style.backgroundcolor = 'blue';
+}
+function red(){
+  
+}
+function undo(){
+  
+}
